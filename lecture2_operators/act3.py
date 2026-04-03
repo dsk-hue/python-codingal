1562b0534ab69863f0501f5fa3bf42a0e2a075ee
dsk-hue <dharshini.santhosh2k@gmail.com>
2026-04-03 18:57:42 +0530

    updates

---
+++ lecture2_operators/act3.py
@@ -0,0 +1,14 @@
+first_name = "JOHN"
+last_name = "DOE"
+full_name = first_name + " " + last_name
+multiplied_name = full_name * 3
+
+print(first_name)
+print(last_name)
+print(full_name)
+print(multiplied_name)
+
+print("Length of Full Name:", len(full_name))
+print("First Letter of Full Name:", full_name[0])
+print("Last Letter of Full Name:", full_name[-1])
+print("Substring of Full Name:", full_name[1:4])
