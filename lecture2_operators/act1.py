c16907246767177078989d08e64081c1caf51572
dsk-hue <dharshini.santhosh2k@gmail.com>
2026-04-03 18:37:43 +0530

    first commit

---
+++ lecture2_operators/act1.py
@@ -0,0 +1,16 @@
+name = "Penguin"
+age = 15
+is_student = True
+weight = 70.5
+
+print("Name:", name)
+print("Data type of name:", type(name))
+
+print("Age:", age)
+print("Data type of age:", type(age))
+
+print("Is student:", is_student)
+print("Data type of is_student:", type(is_student))
+
+print("Weight:", weight)
+print("Data type of weight:", type(weight))
