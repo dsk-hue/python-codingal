9983048b8f0266bca99b09e8bf55cfb2d43dde3f
dsk-hue <dharshini.santhosh2k@gmail.com>
2026-04-03 18:53:20 +0530

    updates

---
+++ lecture2_operators/act2.py
@@ -0,0 +1,26 @@
+num1 = 43
+num2 = 12
+
+print("Number 1:", num1)
+print("Number 2:", num2)
+
+#Arithmetic Operators
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+print("Modulus:", num1 % num2)
+print("Exponentiation:", num1 ** num2)
+print("Floor Division:", num1 // num2)
+print("Square Root of Number 1:", num1 ** 0.5)
+print("Square of Number 1:", num1 ** 2)
+
+#Conditional Operators
+print(num1 == num2)
+print(num1 != num2)
+print(num1 > num2)
+print(num1 < num2)
+print(num1 >= num2)
+print(num1 <= num2)
+ 
